188228f11564494a8c09e9d5d30bd66537ea3b29
Liquidwe <lichengbing9027@gmail.com>
2024-07-05 00:24:07 +0800

    update

--- demo/panic_and_impl/src/main.rs
+++ demo/panic_and_impl/src/main.rs
@@ -2,24 +2,57 @@ use std::cmp::Ordering;
 use std::io;
 use rand::Rng;
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Result<Guess, String> {
+        if value < 1 || value > 100 {
+            Err(format!("Guess value must be between 1 and 100, got {}", value))
+        } else {
+            Ok(Guess { value })
+        }
+    }
+
+    // 提供一个公共的方法来访问私有 Guess 里面的 value 的值
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
-    println!("lets guess number!");
-    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("Let's guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
     loop {
-        println!("guess number:");
+        println!("Guess the number:");
+
         let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("error..");
-        let guess: u32 = match guess.trim().parse() {
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+        let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => {
+                println!("Please enter a valid number!");
+                continue;
+            }
         };
-        println!("you number: {}", guess);
-        match guess.cmp(&secret_number) {
+
+        let guess = match Guess::new(guess) {
+            Ok(g) => g,
+            Err(err) => {
+                println!("{}", err);
+                continue;
+            }
+        };
+
+        match guess.value().cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
-                println!("You Win!");
-                break
+                println!("You win!");
+                break;
             }
         }
     }
